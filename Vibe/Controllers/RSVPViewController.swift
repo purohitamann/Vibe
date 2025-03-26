b620465143e64e840887e5ad4a03a06bab7a16f2
Aman Purohit <122639076+purohitamann@users.noreply.github.com>
2025-03-26 19:33:58 -0400

    Initial Commit

---
+++ Vibe/Controllers/RSVPViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RSVPViewController.swift
+//  Vibe
+//
+//  Created by Aman Purohit on 2025-03-26.
+//
+
+import UIKit
+
+class RSVPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Vibe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vibe
+//
+//  Created by Aman Purohit on 2025-03-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
