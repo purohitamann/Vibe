f24ed29768f0e0706c730d68181a659ac5531144
pareshmi <133808943+pareshmi@users.noreply.github.com>
2025-04-14 23:04:24 -0400

    Commented EventUpdateViewController.swift

--- Vibe/Controllers/EventUpdateViewController.swift
+++ Vibe/Controllers/EventUpdateViewController.swift
@@ -3,33 +3,34 @@
 //  Vibe
 //
 //  Created by Reshmi Patel on 14/04/25.
-//
+// In this Controller, event data can be updated and deleted by the user. 
 
 import UIKit
 import FirebaseFirestore
 
 class EventUpdateViewController: UIViewController {
-
+   //Outlets
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var locationTextField: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
-
+//Array of events fetched from database
     var event: Event?
-
+// this will be called after view is loaded
     override func viewDidLoad() {
         super.viewDidLoad()
+        //will set border style
         setupUI()
-      
+      //UI will be updated based on fetched data
         loadEventData()
     }
-
+//Using this to apply stylying to textview
     func setupUI() {
         descriptionTextView.layer.borderWidth = 1
         descriptionTextView.layer.borderColor = UIColor.gray.cgColor
         descriptionTextView.layer.cornerRadius = 6
     }
-
+//when event is selected this fill the UI with the data
     func loadEventData() {
         guard let event = event else { return }
 
@@ -38,7 +39,7 @@ class EventUpdateViewController: UIViewController {
         locationTextField.text = event.location
         descriptionTextView.text = event.description
     }
-
+    //when update button is tapped ,this will validate input fields
     @IBAction func updateButtonTapped(_ sender: UIButton) {
         guard let event = event,
               let title = titleTextField.text,
@@ -48,14 +49,14 @@ class EventUpdateViewController: UIViewController {
             return
 
         }
-
+    // will update the directory in firebase with updated data
         let updatedData: [String: Any] = [
             "title": title,
             "date": Timestamp(date: datePicker.date),
             "location": location,
             "description": descriptionTextView.text ?? ""
         ]
-
+    //event document will get updated in firebase
         let db = Firestore.firestore()
         db.collection("events").document(event.id).updateData(updatedData) { error in
             if let error = error {
@@ -67,10 +68,10 @@ class EventUpdateViewController: UIViewController {
             }
         }
     }
-
+// action will be triggered when delete button is tapped
     @IBAction func deleteButtonTapped(_ sender: UIButton) {
         guard let eventId = event?.id else { return }
-
+    //show confirmation and event will be deleted
         let db = Firestore.firestore()
         db.collection("events").document(eventId).delete { error in
             if let error = error {
@@ -83,12 +84,13 @@ class EventUpdateViewController: UIViewController {
         }
        
     }
-
+    // shows alert with a message
     func showAlert(message: String) {
         let alert = UIAlertController(title: "Notice", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
+    // show confirmation alert after deletion
     func deleteEvent() {
             guard let eventId = event?.id else { return }
 
@@ -103,8 +105,8 @@ class EventUpdateViewController: UIViewController {
             }
         }
 
-        // MARK: - Helpers
-
+       
+    // action with alert to return to previous screen
         func showAlertAndPop(message: String) {
             let alert = UIAlertController(title: "Done", message: message, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
